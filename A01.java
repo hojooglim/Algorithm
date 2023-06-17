e7ca0a741ca6c952742bfd08cf67de49e2dbb7c3
hojooglim <hojooglim@gmail.com>
2023-06-17 11:08:17 +0900

    달리기 경주 - 시간초과

--- A01.java
+++ A01.java
@@ -1,24 +1,23 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class A01 {
     public static void main(String[] args) {
-        String[] p = {"a","b","c","d"};
-        String[] c = {"c","c","d"};
+        String[] players = {"a","b","c","d"};
+        String[] callings = {"c","c","d"};
 
 
 
         // 맞는거 같은데 시간초과 뜸.
-        ArrayList<String> arrayList= new ArrayList<>();
-        for (String pp :p){
-            arrayList.add(pp);
-        }
+        ArrayList<String> arrayList= new ArrayList<>(Arrays.asList(players));
         //calling에서 불린 인덱스를 찾고
         //지운다음에 인덱스-1에 다시 넣어준다.
-        for (int i = 0 ; i < c.length ; i++){
-            int idx = arrayList.indexOf(c[i]);
+        for (int i = 0 ; i < callings.length ; i++){
+            int idx = arrayList.indexOf(callings[i]);
             arrayList.remove(idx);
             int replaceidx = idx-1;
-            arrayList.add(replaceidx,c[i]);
+            arrayList.add(replaceidx,callings[i]);
+            System.out.println(arrayList);
         }
 
     }
